b764c7bfebea9a5fad1d873c2bd8c5b21c72cb60
bittu <thedevbittu@gmail.com>
2024-07-24 08:43:11 +0530

    add static file

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,7 +122,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'  # see site settings before editing
+if DEBUG:  # see template tag webp before editing
+    STATICFILES_DIRS = [
+        BASE_DIR / 'static',
+    ]
+else:
+    STATIC_ROOT = 'static/'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
