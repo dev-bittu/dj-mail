6e029a7b7990159f20d6a9e918710e67b8babe42
bittu <thedevbittu@gmail.com>
2024-07-24 08:19:41 +0530

    jdksf

--- mail/mails.py
+++ mail/mails.py
@@ -1,4 +1,7 @@
-def send_email(to_email, data):
+from django.core.mail import send_mail
+from django.conf import settings
+
+def send_email(to_email, subj, data):
     msg = "Hi,\n\n"
     msg += "I hope this email finds you well.\n\n"
     msg += "We have received a new inquiry through the inquiry form. Below are the details submitted by the potential client:\n"
@@ -12,3 +15,10 @@ def send_email(to_email, data):
     msg += "Best regards,\n"
     msg += "CyberKernel (bit.ly/cyberkernel)"
   
+    return send_mail(
+        subj,
+        msg,
+        settings.EMAIL_HOST_USER,
+        [to_email],
+        fail_silently=True,
+    )
--- mail/views.py
+++ mail/views.py
@@ -10,10 +10,10 @@ def send_mail(request, uid):
         if mail.exists():
             mail = mail[0]
             data = request.POST
-            res = send_email(mail.email, data)
+            res = send_email(mail.email, mail.subject, data)
             
             return JsonResponse({
-                "msg": "Done"
+                "msg": "done" if res == 1 else "failed"
             })
     
     return redirect("https://bit.ly/cyberkernel")
